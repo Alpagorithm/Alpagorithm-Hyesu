4406a70d694e2a9fb5e5c84a180a7e6c478d80e6
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-02-20 18:23:46 +0900

    #8 잘못된 코드 수정

--- PythonCodingTest/정렬/6-12.py
+++ PythonCodingTest/정렬/6-12.py
@@ -5,5 +5,10 @@ b = list(map(int, input().split()))
 a.sort()
 b.sort()
 
-for i in range(n):
-    print(b[n-1-i])
+for i in range(k):
+    if a[i] < b[n-1-i]:
+        a[i], b[n-1-i] = b[n-1-i], a[i]
+    else:
+        break
+
+print(sum(a))
