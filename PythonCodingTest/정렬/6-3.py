f71a9552219a05c1bee8678f1bb7a1dd43cfffe7
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-02-20 15:13:54 +0900

    #8 이코테 - 삽입정렬

---
+++ PythonCodingTest/정렬/6-3.py
@@ -0,0 +1,11 @@
+# 삽입정렬
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1): # 인덱스 i부터 0까지 -1씩 감소
+        if array[j] < array[j-1]: # 한 칸씩 왼쪽으로 이동
+            array[j], array[j-1] = array[j-1], array[j]
+        else: # 자기보다 작은 데이터를 만나면 멈춤
+            break
+print(array)
